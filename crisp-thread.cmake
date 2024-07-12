cc4f8dbd85bee7ad1e8bb44c16cf620c9fea261f
gunnerDgd <ifstudiomail@naver.com>
2024-07-12 18:53:55 +0900

    [Feature] Now requires CRISP_TLS for crisp-thread usage

--- crisp-thread.cmake
+++ crisp-thread.cmake
@@ -1,5 +1,10 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
 
+if   (NOT CRISP_TLS)
+    message("[crisp-thread] crisp-thread requires CRISP_TLS option for thread local storage support.")
+    message(SEND_ERROR)
+endif()
+
 macro   (crisp_thread_examples)
     add_subdirectory(lib/crisp-thread/examples)
 endmacro()
